e1043bcda3a5e9c7ccb9ced6cff2db1a35c70bb5
Luis Martinez <luismartinezwebdev@gmail.com>
2023-01-09 01:41:08 +0000

    feat: fishbot works

---
+++ fishbot.js
@@ -0,0 +1,59 @@
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const { MODELS } = require("./constants/models");
+const { appendToFile, getUserInput } = require("./util");
+
+const BOT_NAME = 'FLO'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+function makeLog(prompt, response) {
+  return `${cleanText(prompt)}\n${cleanText(response)}\n`;
+}
+
+async function gtp3Completion(openaiOptions = {}) {
+  const defaultOpenaiOptions = {
+    model: MODELS.babbage,
+    max_tokens: 400,
+    temperature: 1,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    stop: ["\n"],
+    echo: false,
+    n: 1,
+    user: 'developer-testing'
+  }
+
+  const options = { ...defaultOpenaiOptions, ...openaiOptions };
+
+  try {
+    const completion = await openai.createCompletion(options);
+
+    return completion.data.choices[0].text.trim();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function cleanText(text) {
+  return text.replace(/\n+/g, ' ').replace(/\s+/g, ' ').trim();
+}
+
+// a chatbot without any memory
+const fishbot = async () => {
+  while (true) {
+    const userInput = await getUserInput('USER: ');
+    const prompt = `USER: ${cleanText(userInput)}\n${BOT_NAME}:`
+    const response = await gtp3Completion({ prompt, stop: [`${BOT_NAME}:`, 'USER:'], temperature: 0.3 })
+    console.log(`${BOT_NAME}: ${response}`)
+    const log = makeLog(`USER: ${cleanText(userInput)}`, `${BOT_NAME}: ${cleanText(response)}`);
+    await appendToFile('./logs', 'fishbot.txt', log);
+  }
+}
+
+fishbot()
--- util.js
+++ util.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const readline = require('readline')
 
 async function readFile(path) {
   try {
@@ -28,8 +29,24 @@ async function appendToFile(path, filename, data) {
   }
 }
 
+
+async function getUserInput(question) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question(question, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
+
 module.exports = {
   readFile,
   writeToFile,
   appendToFile,
+  getUserInput
 };
