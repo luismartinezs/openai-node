2f500ad2da9cee74d8c943c7af83b34c02877ff3
Luis Martinez <luismartinezwebdev@gmail.com>
2023-01-12 00:34:01 +0000

    feat: add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     "env": {
         "browser": true,
@@ -5,7 +6,7 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended", "prettier"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
